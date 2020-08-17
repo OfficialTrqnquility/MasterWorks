bbbf86ff9210cdbe26681741ed23d5154c2b7405
OfficialTrqnquility <trqnquility@gmail.com>
2020-08-16 20:51:37 -0700

    Sprite Sheet implementation complete.

---
+++ src/main/java/me/trqnquility/masterworks/gfx/SpriteSheet.java
@@ -0,0 +1,25 @@
+package me.trqnquility.masterworks.gfx;
+
+import java.awt.image.BufferedImage;
+
+public class SpriteSheet {
+
+    private final BufferedImage sheet;
+    private final int width, height;
+
+    public SpriteSheet(BufferedImage sheet, int width, int height) {
+        this.sheet = sheet;
+        this.width = width;
+        this.height = height;
+    }
+
+    public BufferedImage crop(int x, int y) {
+        return crop(x, y, width, height);
+    }
+
+    private BufferedImage crop(int x, int y, int width, int height) {
+        return sheet.getSubimage(x, y, width, height);
+    }
+
+
+}
