728a27fd88b0fbd0ee2171aa45b18ec076f0c4e7
OfficialTrqnquility <trqnquility@gmail.com>
2020-10-13 10:58:05 -0700

    Added TileManager classes.

---
+++ src/main/java/me/trqnquility/masterworks/tiles/TileManager.java
@@ -0,0 +1,23 @@
+package me.trqnquility.masterworks.tiles;
+
+import lombok.Getter;
+import me.trqnquility.masterworks.exceptions.NoTileException;
+
+@Getter
+public class TileManager {
+
+    private static final TileManager instance = new TileManager();
+
+    private final Tile[] tiles;
+
+    public TileManager() {
+        tiles = new Tile[256];
+    }
+
+    public Tile getTile(int id) throws NoTileException {
+        Tile tile = tiles[id];
+        if (tile == null) throw new NoTileException("Tile " + id + " does not exist.");
+        return tile;
+    }
+
+}
