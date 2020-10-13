efd0ec9f40dac967d4c7c6477a0f327e878366ec
OfficialTrqnquility <trqnquility@gmail.com>
2020-10-13 10:57:23 -0700

    Added 2 exceptions, NoTileException and NoStateException used for when the tile you try to get isn't a real tile and when the State isn't set.

---
+++ src/main/java/me/trqnquility/masterworks/exceptions/NoStateException.java
@@ -0,0 +1,8 @@
+package me.trqnquility.masterworks.exceptions;
+
+public class NoStateException extends Exception {
+
+    public NoStateException(String errorMessage) {
+        super(errorMessage);
+    }
+}
---
+++ src/main/java/me/trqnquility/masterworks/exceptions/NoTileException.java
@@ -0,0 +1,8 @@
+package me.trqnquility.masterworks.exceptions;
+
+public class NoTileException extends Exception{
+
+    public NoTileException(String errorMessage) {
+        super(errorMessage);
+    }
+}
