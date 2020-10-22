b85b8d86e1c37621444c7b2857bbdba8a5cf9a02
Aethonx <aethonxn@gmail.com>
2020-10-22 10:51:57 -0500

    Entity health is not applicable to the base entity.

--- src/main/java/me/trqnquility/masterworks/entity/model/Entity.kt
+++ src/main/java/me/trqnquility/masterworks/entity/model/Entity.kt
@@ -5,7 +5,6 @@ import me.trqnquility.masterworks.location.Position
 
 interface Entity {
     fun entityId(): Int
-    fun entityHealth(): Double
     fun entityBoundingBox(): BoundingBox
     fun entityPosition(): Position
 }
