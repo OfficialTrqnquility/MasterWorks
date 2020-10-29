6c2cc0ed1724c03fcd9a3235ccc4848812f4d1f6
Aethonx <45923326+aethonx@users.noreply.github.com>
2020-10-29 11:04:47 -0500

    Push Registry

---
+++ src/main/java/me/trqnquility/masterworks/entity/registry/EntityRegistryEntry.kt
@@ -0,0 +1,7 @@
+package me.trqnquility.masterworks.entity.registry
+
+import me.trqnquility.masterworks.entity.model.living.LivingEntityType
+
+class EntityRegistryEntry(val type: LivingEntityType) {
+
+}
